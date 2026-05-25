61ccc166aa366d06e9e6be2f013b6a06d0c12255
yueyi <email@weiwenhuaming.com>
2026-05-25 22:44:13 +0800

    feat:category

---
+++ gateway/internal/handler/category/list_categories_handler.go
@@ -0,0 +1,28 @@
+package category
+
+import (
+	"net/http"
+
+	"gateway/internal/logic/category"
+	"gateway/internal/svc"
+	"gateway/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func ListCategoriesHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.ListCategoriesReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := category.NewListCategoriesLogic(r.Context(), svcCtx)
+		resp, err := l.ListCategories(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
---
+++ gateway/internal/logic/category/delete_category_logic.go
@@ -0,0 +1,52 @@
+package category
+
+import (
+	"context"
+	"core-rpc/core_client"
+
+	"gateway/internal/svc"
+	"gateway/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type DeleteCategoryLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewDeleteCategoryLogic(ctx context.Context, svcCtx *svc.ServiceContext) *DeleteCategoryLogic {
+	return &DeleteCategoryLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *DeleteCategoryLogic) DeleteCategory(req *types.DeleteCategoryReq) (resp *types.DeleteCategoryResp, err error) {
+	_ = req
+	if req == nil || req.Id == 0 {
+		return &types.DeleteCategoryResp{
+			Code: 400,
+			Msg:  "id is required",
+		}, nil
+	}
+
+	_, err = l.svcCtx.Core.DeleteCategory(l.ctx, &core_client.DeleteCategoryReq{
+		Id: req.Id,
+	})
+	if err != nil {
+		return &types.DeleteCategoryResp{
+			Code: 500,
+			Msg:  err.Error(),
+		}, nil
+	}
+
+	return &types.DeleteCategoryResp{
+		Code: 200,
+		Msg:  "ok",
+	}, nil
+
+	return
+}

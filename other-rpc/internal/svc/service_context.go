20b776d583b8e45789324ba84d9b636f18656fe9
cxy <492730753@qq.com>
2026-06-09 13:28:30 +0800

    refactor：Rebuild directory tree

--- other-rpc/internal/svc/service_context.go
+++ other-rpc/internal/svc/service_context.go
@@ -2,7 +2,7 @@ package svc
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"other-rpc/internal/agent"
 	"other-rpc/internal/agent/embedding"
 	"other-rpc/internal/agent/vector"
@@ -21,22 +21,22 @@ type ServiceContext struct {
 func NewServiceContext(c config.Config) *ServiceContext {
 	embedder, err := embedding.NewEmbedder(c.KnowledgeBase.Embedding)
 	if err != nil {
-		fmt.Println("初始化 Embedding 模块失败:", err)
+		log.Fatal("初始化 Embedding 模块失败:", err)
 	}
 
 	ch, err := vector.NewChroma(context.Background(), c.KnowledgeBase)
 	if err != nil {
-		fmt.Println("Chroma 初始化失败:", err)
+		log.Fatal("Chroma 初始化失败:", err)
 	}
 
 	retriever, err := ch.Load(context.Background(), embedder)
 	if err != nil {
-		fmt.Println("vector 初始化失败:", err)
+		log.Fatal("vector 初始化失败:", err)
 	}
 
 	kbAgent, err := agent.NewAgent(c.KnowledgeBase, retriever)
 	if err != nil {
-		fmt.Println("Agent 构建失败:", err)
+		log.Fatalf("Agent 构建失败:", err)
 	}
 
 	return &ServiceContext{

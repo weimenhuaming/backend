4f04a28851d3bf16349d7c1f079885aac99de884
yueyi <email@weiwenhuaming.com>
2026-06-19 20:39:53 +0800

    feat:add search function

--- gateway/internal/logic/article/search_articles_logic.go
+++ gateway/internal/logic/article/search_articles_logic.go
@@ -2,9 +2,14 @@ package article
 
 import (
 	"context"
+	"strings"
 
+	core_client "core-rpc/core_client"
+	"gateway/internal/response"
 	"gateway/internal/svc"
 	"gateway/internal/types"
+	"gateway/internal/utils/converter"
+	"gateway/internal/utils/vaild"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -24,7 +29,27 @@ func NewSearchArticlesLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Se
 }
 
 func (l *SearchArticlesLogic) SearchArticles(req *types.SearchArticlesReq) (resp *types.SearchArticlesData, err error) {
-	// todo: add your logic here and delete this line
+	keyword := strings.TrimSpace(req.Keyword)
+	if keyword == "" {
+		return nil, response.ErrorBadRequest("搜索关键词不能为空")
+	}
 
-	return
+	page, pageSize := vaild.NormalizePageSize(req.Page, req.PageSize)
+
+	r, err := l.svcCtx.Core.SearchArticles(l.ctx, &core_client.SearchArticlesReq{
+		Keyword:    keyword,
+		Page:       page,
+		PageSize:   pageSize,
+		CategoryId: req.CategoryId,
+	})
+	if err != nil {
+		return nil, response.ErrorInternalServer(err.Error())
+	}
+
+	return &types.SearchArticlesData{
+		Articles: converter.ToArticleList(r.GetArticles()),
+		Total:    r.GetTotal(),
+		Page:     r.GetPage(),
+		PageSize: r.GetPageSize(),
+	}, nil
 }
